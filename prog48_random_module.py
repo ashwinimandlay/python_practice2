3d8d99cdc5734f04abb43453f3a6f764d9a7b10b
ashwinimandlay <104056898+ashwinimandlay@users.noreply.github.com>
2024-02-27 02:14:41 +0530

    Update prog48_random_module.py

--- prog48_random_module.py
+++ prog48_random_module.py
@@ -4,10 +4,10 @@ import random
 
 for i in range(3):
     print(random.random())
-    # random value between 0 and 1
+    # random float value between 0 and 1
 for i in range(3):
     print(random.randint(10, 20))
-    # random value between 10 and 20
+    # random int value between 10 and 20
 
 # we can also select a random person from a group
 members = ['josh', 'mosh', 'bob', 'mary']
